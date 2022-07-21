5eb5a0c86c38440b93378857512440141c1c59d8
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-07-21 19:52:00 +0300

    use of android libraries.

---
+++ app/src/main/java/com/example/mycontacts/ViewContactActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mycontacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class ViewContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_contact)
+        getExtras()
+    }
+    fun getExtras(){
+        var name=intent.extras?.getString("NAME")
+        var address=intent.extras?.getString("ADDRESS")
+        Toast.makeText(this,name,Toast.LENGTH_LONG).show()
+        Toast.makeText(this,address,Toast.LENGTH_LONG).show()
+    }
+}
--- app/src/main/java/com/example/mycontacts/contactRvAdaptor.kt
+++ app/src/main/java/com/example/mycontacts/contactRvAdaptor.kt
@@ -1,10 +1,12 @@
 package com.example.mycontacts
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mycontacts.databinding.ContactListTemBinding
 import com.squareup.picasso.Picasso
@@ -31,13 +33,21 @@ class contactRvAdaptor(var contactList:List<Contacts>):
             .centerCrop()
             .placeholder(R.drawable.ic_baseline_person_24)
             .into(holder.binding.imgcontact)
+        val context=holder.itemView.context
+        holder.binding.imgcontact.setOnClickListener{
+            Toast.makeText(context,"You have clicked on the image",Toast.LENGTH_SHORT).show()
 
+        }
+        holder.binding.cvContact.setOnClickListener{
+            val intent=Intent(context,ViewContactActivity::class.java)
+            intent.putExtra("NAME",currentContact.name)
+            intent.putExtra("ADDRESS",currentContact.address)
+            context.startActivity(intent)
+        }
     }
-
     override fun getItemCount(): Int {
      return contactList.size
     }
-
 }
 class ContactViewHolder(val binding:ContactListTemBinding):
         RecyclerView.ViewHolder(binding.root){
